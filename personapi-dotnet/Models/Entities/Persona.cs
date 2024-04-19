41ec5e21bfbbd525f8e5a708cac6977ae9f46afe
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-04-19 00:23:24 -0500

    complete

---
+++ personapi-dotnet/Models/Entities/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace personapi_dotnet.Models.Entities;
+
+public partial class Persona
+{
+    public int Cc { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Genero { get; set; } = null!;
+
+    public int? Edad { get; set; }
+
+    public virtual ICollection<Estudio> Estudios { get; set; } = new List<Estudio>();
+
+    public virtual ICollection<Telefono> Telefonos { get; set; } = new List<Telefono>();
+}
