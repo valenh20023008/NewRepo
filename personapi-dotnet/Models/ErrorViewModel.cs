4b30847d28f617b05a53ff9a1226e223ea36547c
Valentina Hernandez Bayona <102444028+valenh20023008@users.noreply.github.com>
2024-04-18 22:58:35 -0500

    Delete personapi-dotnet directory

--- personapi-dotnet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace personapi_dotnet.Models
-{
-  public class ErrorViewModel
-  {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
